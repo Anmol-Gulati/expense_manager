dc8204e2693a65b090f8d83c6a1e93cfd01052fb
Anmol-Gulati <anmol.gulati9513@gmail.com>
2017-04-05 20:31:43 +0530

    Added fuctionality for adding expense.

--- expense/views.py
+++ expense/views.py
@@ -1,5 +1,25 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from . import models
 
+@login_required
 def add_expense(request):
+	if request.method == 'POST':
+		category = request.POST.get('category')
+		description = request.POST.get('description')
+		amount = request.POST.get('amount')
+
+		models.Expense.objects.create(
+			category=category,
+			description=description,
+			amount=amount,
+			user=request.user
+		)
+
+		success_message = "Expense Successfully Added."
+
+		return render(request, 'expense/expense_form.html', {'success_message': success_message})
+
 	return render(request, 'expense/expense_form.html')
