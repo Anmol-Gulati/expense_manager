cb53d0cc813c6573bd42b3c5b673e0ead5add350
Anmol-Gulati <anmol.gulati9513@gmail.com>
2017-04-05 20:03:19 +0530

    Added layout for adding expense.

--- expense/urls.py
+++ expense/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-		
+	url(r'add/', views.add_expense, name='add_expense'),	
 ]
--- expense/views.py
+++ expense/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 
+def add_expense(request):
+	return render(request, 'expense/expense_form.html')
